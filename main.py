72147644441d42a194a98d392c6ceb34f6884c95
smart0120 <gadrgon0305@gmail.com>
2020-12-13 12:06:35 -0300

    main script initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Script for loading a list of files on google drive and store info to ms sql db
+"""
+
+import os
+from dotenv import load_dotenv
+
+__author__ = "bursno22"
+__license__ = "MIT"
+__version__ = "0.0.1"
+
+load_dotenv()
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
